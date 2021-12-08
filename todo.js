1b662207555b3bf839643e9c75212bb6b06e624a
Hazem Kawas <hazem.k.4488@gmail.com>
2021-12-08 15:51:38 +0100

    adding days divs via js

--- todo.js
+++ todo.js
@@ -1,5 +1,7 @@
+
+
 let today = new Date();
-let date = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
-document.querySelector('.displayDate').innerHTML = `${date}`;
+let dat = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
+document.querySelector('.displayDate').innerHTML = `${dat}`;
 let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 document.querySelector('.displayTime').innerHTML = `${time}`;

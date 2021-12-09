8304422295659f55accbcffab42f4c49d3a91403
Hazem Kawas <hazem.k.4488@gmail.com>
2021-12-09 10:25:44 +0100

    giteeing the calander to work

--- calender.js
+++ calender.js
@@ -1,42 +1,38 @@
 window.addEventListener('load', main);
 
 const date = new Date();
-console.log(date);
 
-const year = date.getFullYear();
-console.log(year);
+// const year = date.getFullYear();
+// console.log(year);
 
-const month = date.getMonth() + 1;
-console.log(month);
+// const month = date.getMonth() + 1;
+// console.log(month);
 
-const day = date.getDate();
-console.log(day);
+// const day = date.getDate();
+// console.log(day);
 
 function main() {
     renderCalenders()
+    addEventsListeners()
 }
 
 const renderCalenders = () => {
     date.setDate(1)
-
-    const monthDays = document.querySelector('.daysOfTheMonth');
+    const monthDays = document.querySelector('.days');
 
     const lastDay = new Date(
         date.getFullYear(),
         date.getMonth() + 1,
         0
         ).getDate();
-        console.log('last', lastDay);
 
-    const prevDay = new Date(
+    const prevLastDay = new Date(
             date.getFullYear(),
             date.getMonth(),
             0
         ).getDate();
-        console.log(prevDay);
 
-    const firstDayIndex = date.getDate();
-        console.log(firstDayIndex);
+    const firstDayIndex = date.getDay();
 
 
     const lastDayIndex = new Date(
@@ -44,10 +40,8 @@ const renderCalenders = () => {
             date.getMonth() + 1,
             0
         ).getDay();
-        console.log(lastDayIndex);
 
     const nextDays = 7 - lastDayIndex - 1;
-        console.log(nextDays);
 
     const months = [
             "January",
@@ -63,25 +57,74 @@ const renderCalenders = () => {
             "November",
             "December",
           ];
+    const weekDays = [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+        ];
+         
+    document.querySelector('.displayDay').innerHTML = weekDays[date.getDay()];
+    let todayDate = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
+    document.querySelector('.displayDate').innerHTML = `${todayDate}`;
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    document.querySelector('.displayTime').innerHTML = `${time}`;
 
-    document.querySelector('.displayMonth h2').innerHTML = months[date.getMonth()];
+    document.querySelector('.date h2').innerHTML = months[date.getMonth()];
+    document.querySelector('.date p').innerHTML =  todayDate;
+
+   
         
     let days = '';
-        for (let i = 0; i < lastDay; i++) {
-            const daysOfTheMonth = document.querySelector('.daysOfTheMonth');
-            days = document.createElement('div');
-            days.className = "days";
-            daysOfTheMonth.appendChild(days);
-            days.innerHTML = day;
-        }
-
-    document.querySelector('.prev').addEventListener('click', () => {
-            date.setMonth(date.getMonth() - 1);
-            renderCalenders();
-        })
-    document.querySelector('.next').addEventListener('click', () => {
-            date.setMonth(date.getMonth() + 1);
-            renderCalenders();
-        })
-
+    
+    for (let x = firstDayIndex; x > 0; x--) {
+        days += `<div class = 'prev-date day'>${prevLastDay - x + 1}</div>`;
+        
+    }
+    for (let i = 1; i <= lastDay; i++) {
+        if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+            days += `<div class="today day">${i}</div>`;
+        } else {
+        days += `<div class="day">${i}</div>`;
+    }
+    }
+    for (let j = 1; j <= nextDays; j++) {
+        days += `<div class="next-date day">${j}</div>`;
+        monthDays.innerHTML = days; 
+    }
 }
+
+function addEventsListeners() {
+    document.querySelector('.prev').addEventListener('click', () => {
+        date.setMonth(date.getMonth() - 1);
+        renderCalenders();
+    });
+    document.querySelector('.next').addEventListener('click', () => {
+        date.setMonth(date.getMonth() + 1);
+        renderCalenders();
+    });
+
+
+    const dayBoxes = document.querySelectorAll('.day');
+    for (const dayBox of dayBoxes) {
+        dayBox.addEventListener('click', () => {
+            console.log("yes");
+            let todoList = document.getElementById('todo');
+            todoList.classList.toggle('active');
+        })
+        
+    }
+}
+
+
+
+
+
+// todo
+
+
+
+let today = new Date();
--- todo.js
+++
@@ -1,7 +0,0 @@
-
-
-let today = new Date();
-let dat = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
-document.querySelector('.displayDate').innerHTML = `${dat}`;
-let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-document.querySelector('.displayTime').innerHTML = `${time}`;

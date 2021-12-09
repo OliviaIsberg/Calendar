66a529d802d8c5e338e7dd6ec4cf5bdde4696d27
A-Ozmehak <anna.ozmehak@gmail.com>
2021-12-09 13:37:09 +0100

    fixed todays day #3

--- todo.js
+++ todo.js
@@ -1,43 +1,52 @@
 window.addEventListener('load', clock);
+
 function clock() {
-startClock();
+    startClock();
 }
+
 /**
 * Keeps the time updated
 */
 function startClock() {
-setInterval(renderClock, 1000);
+    setInterval(renderClock, 1000);
 }
+
 /**
 * Updates the page with current time and weekday
 */
 function renderClock() {
-const currentDate = new Date();
-const weekdayElement = document.getElementsByClassName('displayDay');
-weekdayElement.innerHTML = getWeekDay(currentDate);
-let today = new Date();
-let todaysDate = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
-document.querySelector('.displayDate').innerHTML = `${todaysDate}`;
-const timeElement = document.querySelector('.displayTime');
-timeElement.innerHTML = getCurrentTime(currentDate);
+    const currentDate = new Date();
+
+    const weekdayElement = document.querySelector('.displayDay');
+    weekdayElement.innerHTML = getWeekDay(currentDate);
+
+    let today = new Date();
+    let todaysDate = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
+    document.querySelector('.displayDate').innerHTML = `${todaysDate}`;
+
+    const timeElement = document.querySelector('.displayTime');
+    timeElement.innerHTML = getCurrentTime(currentDate);
 }
+
 /**
 * Takes a date and returns current weekday
 * @param {Date} currentDate
 * @returns {String}
 */
 function getWeekDay(currentDate) {
-const weekDay = currentDate.getDay();
-switch(weekDay) {
-case 0: return 'Söndag';
-case 1: return 'Måndag';
-case 2: return 'Tisdag';
-case 3: return 'Onsdag';
-case 4: return 'Torsdag';
-case 5: return 'Fredag';
-case 6: return 'Lördag';
+    const weekDay = currentDate.getDay();
+
+    switch(weekDay) {
+    case 0: return 'Söndag';
+    case 1: return 'Måndag';
+    case 2: return 'Tisdag';
+    case 3: return 'Onsdag';
+    case 4: return 'Torsdag';
+    case 5: return 'Fredag';
+    case 6: return 'Lördag';
 }
 }
+
 /**
 * Constructs the time string from a date, with seconds
 * @param {Date} currentDate

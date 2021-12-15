4a0faee66e37a5c6111e2f98951d2fe74c50d66a
A-Ozmehak <anna.ozmehak@gmail.com>
2021-12-15 10:39:54 +0100

    added localstorage

--- todo.js
+++ todo.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', mainToDo);
 
 function mainToDo() {
+    loadFromLS();
     addEventListeners();
     addtTodoToList()
-    addTodo()
     deleteTodoFromList()
 }
 
@@ -14,7 +14,7 @@ function addEventListeners() {
     toggleButton.addEventListener('click', toggleForm)
     let test = document.querySelector('form');
     test.addEventListener('submit', addTodo);
-    
+
 }
 
 function inputFieldToDoList(e) {
@@ -26,8 +26,8 @@ function inputFieldToDoList(e) {
 function toggleForm() {
     const form = document.getElementsByClassName('todo-box')[0];
     form.classList.toggle('hide')
-    
-    
+
+
 }
 
 
@@ -39,64 +39,70 @@ function addtTodoToList() {
 
     // loopa igenom arrayen med "todo" objekten
     for (const todo of todos) {
-        
+
         //skapar ett list-element ("Li") för varje  objekt "todo" ur arrayen todos och skriver ut "title" ur objektet i DOMen. samt
         // lägger till en knapp som har funktion on click. 
         const liTodo = document.createElement("li");
         liTodo.innerHTML = todo.title + `<button onclick=deleteTodoFromList() class="deleteTodo">X</button>`;
         liTodo.className = "list-item"
-       
-        
+
+
         // lägg till li-elementet i UL'en
         ulTodo.append(liTodo);
-    }  
+    }
+}
+
+function loadFromLS() {
+
+
+    let todoList = JSON.parse(localStorage.getItem('listOfToDo'));
+
+    console.log(todoList)
+    todoList.forEach(element => {
+        todos.push(element)
+    })
 }
 
 
 //funktionen som gör så att ett nytt list-element skapas. 
-function addTodo(){
+function addTodo() {
     let todo = document.getElementById('text');
     console.log(todo.value);
-    todos.push ({title:todo.value, date: new Date()});//nu så läggs "date" objektet till med datumet det är i nutid när man trycker på knappen.
+    todos.push({title: todo.value, date: new Date()});//nu så läggs "date" objektet till med datumet det är i nutid när man trycker på knappen.
     addtTodoToList()                                  // det kvarstår nu att lösa så att den lägg tills med rätt datum. 
     console.log(todos)
 
+    localStorage.setItem("listOfToDo", JSON.stringify(todos));
 }
 
 // en funktion som tar bort "todo" från listan samt tar bort onjektet från arrayen. från arrayen. 
-function deleteTodoFromList(){
+function deleteTodoFromList() {
     let buttons = document.getElementsByClassName("deleteTodo")
     console.log(buttons)
 
-  let removeTodo = document.getElementsByClassName("deleteTodo");
+    let removeTodo = document.getElementsByClassName("deleteTodo");
 
-for (let i = 0; i < removeTodo.length; i++) {
-  removeTodo[i].onclick = function(e) {
-    let todo = this.parentNode;
-    todo.style.display = "none";
-    let title = e.target.getAttribute('title');// title = ex "baka en tårta"
-    todos.splice(title, 1);
-    console.log(todos)
-  }
-}
- 
-}
+    for (let i = 0; i < removeTodo.length; i++) {
+        removeTodo[i].onclick = function (e) {
+            let todo = this.parentNode;
+            todo.style.display = "none";
+            let index = e.target.getAttribute('value');// title = ex "baka en tårta"
+            todos.splice(index, 1);
+            console.log(todos)
 
+
+        }
+    }
+
+}
 
 
 const todos = [
     {
         title: 'baka en tårta',
         date: '2021-12-09',
-    },
-    {
-        title: 'baka en tårta',
-        date: '2021-12-09',
-    },    
-    {
-        title: 'baka en tårta',
-        date: '2021-12-10',
     }
+
 ];
 
 console.log(todos)

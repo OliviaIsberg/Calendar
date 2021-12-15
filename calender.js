da5b433404d7c44f12bfc718874287f708c617b0
Olivia isberg <oliviaisberg@gmail.com>
2021-12-15 11:05:09 +0100

    Added function to render the Calendar

--- calender.js
+++ calender.js
@@ -26,4 +26,43 @@ function Calendar() {
         // Append div to the container
         dayContainer.appendChild(this.dateElements[i]);
     }
+
+    this.render();
+}
+
+Calendar.prototype.render = function () {
+    const month = new Date(this.date.getFullYear(), this.date.getMonth()); // Construct New date instance representing the first day of the current month
+    const firstDayOfMonth = month.getDay() === 0 ? 6 : month.getDay() - 1; // Date.getDay but with 0-6 representing monday-sunday
+    const daysInMonth = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0).getDate(); // Gets the last date of the month
+    const daysInPreviousMonth = new Date(this.date.getFullYear(), this.date.getMonth(), 0).getDate(); // Gets the last date of the previous month
+
+    // Sets text and classes for the days belonging to the previous month
+    for (let i = 0; i < firstDayOfMonth; i++) {
+        this.dateElements[i].firstChild.innerText = daysInPreviousMonth - firstDayOfMonth + 1 + i;
+        this.dateElements[i].className = 'day prev-date';
+        this.dateElements[i].getElementsByTagName('p')[1].innerText = '';
+        this.dateElements[i].date = new Date(this.date.getFullYear(), this.date.getMonth() - 1, daysInPreviousMonth - firstDayOfMonth + 1 + i);
+    }
+
+    // Sets text and class for the days belonging to the current month
+    for (let i = 0; i < daysInMonth; i++) {
+        this.dateElements[firstDayOfMonth + i].firstChild.innerText = i + 1;
+        this.dateElements[firstDayOfMonth + i].className = 'day';
+        this.dateElements[firstDayOfMonth + i].getElementsByTagName('p')[1].innerText = '';
+        this.dateElements[firstDayOfMonth + i].date = new Date(this.date.getFullYear(), this.date.getMonth(), i + 1)
+    }
+
+    // Sets text and classes for the days belonging to the next month
+    for (let i = 0; i < 42 - daysInMonth - firstDayOfMonth; i++) {
+        this.dateElements[firstDayOfMonth + daysInMonth + i].firstChild.innerText = i + 1;
+        this.dateElements[firstDayOfMonth + daysInMonth + i].className = 'day next-date';
+        this.dateElements[firstDayOfMonth + daysInMonth + i].getElementsByTagName('p')[1].innerText = '';
+        this.dateElements[firstDayOfMonth + daysInMonth + i].date = new Date(this.date.getFullYear(), this.date.getMonth() + 1, i + 1)
+    }
+
+    // Set the month header
+    document.querySelector('.date h2').innerText = Calendar.Months[this.date.getMonth()];
+
+    // Set the year header
+    document.querySelector('.date p').innerText = this.date.getFullYear();
 }
